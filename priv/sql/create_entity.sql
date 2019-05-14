4dbd7bbdd0639d7b6cd6b627e79821635c147970
Kernelgarden <kernelgarden@gmail.com>
2019-05-14 17:15:54 +0900

    change entity scheme

--- priv/sql/create_entity.sql
+++ priv/sql/create_entity.sql
@@ -2,10 +2,12 @@ CREATE TABLE entity (
     id INT unsigned NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     url VARCHAR(255) NOT NULL,
-    owner_id INT unsigned NOT NULL,
+    ownership_id INT unsigned NOT NULL,
     parent_id INT unsigned NOT NULL,
     thumbnail_url VARCHAR(255) NULL,
     is_tab BOOLEAN NOT NULL,
+    is_public BOOLEAN NOT NULL,
+    version INT unsigned NOT NULL,
     modified_at DATETIME NOT NULL,
     deleted_at DATETIME NULL,
     PRIMARY KEY (id)
